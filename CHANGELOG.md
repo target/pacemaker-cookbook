b1d187ed072b0776c1d0801ace4dc8e880e31aa3
Jacob McCann <jacob.mccann2@target.com>
2016-03-14 10:33:03 -0500

    Version 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ pacemaker CHANGELOG
 
 This file is used to list changes made in each version of the pacemaker cookbook.
 
-0.1.0
+1.0.1
+-----
+- Require `shellwords` library
+
+1.0.0
 -----
 - Initial release of pacemaker
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'Travis.Killoren@Target.com'
 license          'Apache 2.0'
 description      'Installs/Configures pacemaker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 source_url       'https://github.com/target/pacemaker-cookbook'
 issues_url       'https://github.com/target/pacemaker-cookbook/issues'
 
